fe6c23cb04efba30abc2329cd454fdd07aa6382d
Onlinecarrental <car.rental.project.a@gmail.com>
2025-04-19 13:30:08 +0500

    first commit

--- src/views/HomeView/findVehicleForm.jsx
+++ src/views/HomeView/findVehicleForm.jsx
@@ -61,7 +61,7 @@ export default function CarRentalForm() {
 
   return (
     <div className="w-full flex justify-center relative bottom-16 items-center">
-      <BaseCard width="1200px" height="auto" padding="24px">
+      <BaseCard width="max-w-[1280px]" height="auto" padding="24px">
         <div className="grid grid-cols-1 md:grid-cols-5 p-7 gap-4">
           {/* Drop-off Location */}
           <div className="flex flex-col">
